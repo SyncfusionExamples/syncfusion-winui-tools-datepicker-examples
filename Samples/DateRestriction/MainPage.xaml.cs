f4d7211b686fb53e221e6475a6a5490f2bac004c
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-12-30 11:13:21 +0530

    DatePicker UG samples committed.

---
+++ Samples/DateRestriction/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Syncfusion.UI.Xaml.Editors;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DateRestriction
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SfDatePicker_DateFieldItemPrepared(object sender, DateTimeFieldItemPreparedEventArgs e)
+        {
+            //Restrict the weekend days
+            if (e.ItemInfo.DateTime.Value.DayOfWeek == DayOfWeek.Saturday ||
+                    e.ItemInfo.DateTime.Value.DayOfWeek == DayOfWeek.Sunday)
+            {
+                e.ItemInfo.IsBlackout = true;
+            }
+        }
+    }
+}
