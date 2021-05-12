d18843cf0c8cdc28ae04750d396fb03f3fe0427d
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2021-05-12 20:35:36 +0530

    DatePicker view name `CustomUIView` changed to `CustomView`

--- Samples/CustomUI/CustomUI.winui_net50/App.xaml.cs
+++ Samples/CustomUI/CustomUI.winui_net50/App.xaml.cs
@@ -77,7 +77,7 @@ namespace CustomUI
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(CustomUIView), e.Arguments);
+                    rootFrame.Navigate(typeof(CustomView), e.Arguments);
                 }
                 // Ensure the current window is active
                 m_window.Activate();
@@ -112,7 +112,7 @@ namespace CustomUI
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(CustomUIView), e.Arguments);
+                    rootFrame.Navigate(typeof(CustomView), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- Samples/CustomUI/CustomUI.winui_net50/CustomView.xaml.cs
+++ Samples/CustomUI/CustomUI.winui_net50/CustomView.xaml.cs
@@ -21,9 +21,9 @@ namespace CustomUI
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class CustomUIView : Page
+    public sealed partial class CustomView : Page
     {
-        public CustomUIView()
+        public CustomView()
         {
             this.InitializeComponent();
         }
